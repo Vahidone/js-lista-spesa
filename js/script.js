e3a892caa4fa6a8d57073c0a2f8bca014ba08740
Vahidone <v.ban.roma@gmail.com>
2023-09-14 15:16:43 +0200

    feat:  print shoping list on page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+  const shopingList = ["Latte", "Pane", "Uova", "Frutta", "Verdura"];
+        
+  
+  let i = 0;
+  
+  
+  const shopingListPrint = document.getElementById("lista-spesa");
+  
+ 
+  while (i < shopingList.length) {
+      const article = shopingList[i];
+      
+      i++;
+
+      const listItem = document.createElement("li");
+
+      listItem.textContent = article;
+      
+      
+      shopingListPrint.appendChild(listItem);
+      
+      
+  }
